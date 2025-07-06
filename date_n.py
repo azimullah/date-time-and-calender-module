cc57a0d6526049e8386317528030477e05a1ec82
azimullah <mirza.mnnit@gmail.com>
2025-07-06 11:18:18 +0530

    first commit

---
+++ date_n.py
@@ -0,0 +1,8 @@
+from datetime import date, time , datetime
+
+today = date.today()
+now = datetime.now()
+print("Today's date:", today)
+print("Current time:", now) 
+
+print(" date:", today.year, today.month, today.day)
